3aada7051c918e81ae0138755516068f81bb16c0
HamedWS <HamedWosugy@gmail.com>
2023-02-11 18:34:38 +0330

    Add Login Component

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,4 +1,45 @@
+import "../style/login.css"
+import logo from "../assets/logo-big.png"
 const Login = () => {
-  return <div></div>;
+  const userType = "کارفرما"
+  return <div className="login-body">
+    <div className="login-container">
+    <div className="login-wrapper">
+      <div className="login">
+      <div className="login-logo">
+            <img src={logo} alt="login logo"/>
+           </div>
+           <div className="login-header">
+            <span className="login-header-title">
+              ورود | ایجاد حساب
+            </span>
+
+            <span className="login-user-type">{userType}</span>
+           </div>
+           <div className="login-email-form">
+            <label>
+              آدرس ایمیل خودتان را وارد کنید
+            </label>
+            <input type="email" placeholder="example@gmail.com"/>
+            <input type="button" value="ادامه" />
+           </div>
+      </div>
+                 
+           <button className="user-switch">
+            کارفرما هستید؟
+           </button> 
+    </div>
+
+
+   
+
+  </div>
+  </div>
+  
 };
+
+
+function hamed(){
+  
+}
 export default Login;
