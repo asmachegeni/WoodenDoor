f809d4247e87c5ce99aa4cf1d692a38baf3b8cee
asma <as.ch.2280@gmail.com>
2023-02-13 00:05:08 +0330

    add SendingResume component

--- src/component/SendingResume.jsx
+++ src/component/SendingResume.jsx
@@ -1,4 +1,28 @@
-const SendingResume = () => {
-    return <div></div>;
-  };
-  export default SendingResume;
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { FaLock } from "react-icons/fa";
+import './../style/SendingResume.css'
+const SendingResume = ({ isLogin = true }) => {
+  return (
+    <div className="SendingResume">
+      <div className={isLogin ? "SendingResumeContainer" : "SendingResumeContainerLock"}>
+        <span className={isLogin ? "SendingResumeView" : "SendingResumeLock"}>
+          {isLogin ? "مشاهده رزومه" : "برای ارسال رزومه وارد حساب خود شوید"}
+        </span>
+        {isLogin ? (
+          <AiOutlineArrowLeft className="SendingResumeIcon" />
+        ) : (
+          <FaLock className="SendingResumeLockIcon"/>
+        )}
+      </div>
+      <button
+        className="SendingResumeBtn"
+        onClick={() => {
+          console.log("click");
+        }}
+      >
+        ارسال رزومه
+      </button>
+    </div>
+  );
+};
+export default SendingResume;
