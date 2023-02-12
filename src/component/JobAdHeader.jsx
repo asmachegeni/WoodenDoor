4f4d7c164051ad6bd427d3de568c330fcfef6332
asma <as.ch.2280@gmail.com>
2023-02-12 22:25:57 +0330

    change faviocn

--- src/component/JobAdHeader.jsx
+++ src/component/JobAdHeader.jsx
@@ -1,107 +1,88 @@
-import "../style/jobadheader.css"
-import header from "../assets/default-companyHeader.jpeg"
-import logo from "../assets/default-companyLogo.jpg"
-import {BsHeart} from "react-icons/bs"
-import {HiOutlineShare} from "react-icons/hi"
-import {SlArrowLeft} from "react-icons/sl"
+import "../style/jobadheader.css";
+import header from "../assets/img/default-companyHeader.jpeg";
+import logo from "../assets/img/default-companyLogo.jpg";
+import { BsHeart } from "react-icons/bs";
+import { HiOutlineShare } from "react-icons/hi";
+import { SlArrowLeft } from "react-icons/sl";
 const JobAdHeader = () => {
-    return <div className="ad-header-container">
+  return (
+    <div className="ad-header-container">
       <img className="header-cover" src={header} />
       <div className="ad-header-wrapper">
-      <div className="row1">
-        <div className="company-img">
-          <img src={logo}/>
-        </div>
-        <div className="row1-content">
-          <div className="row1-content-row">
-            <div className="job-title-container">
-              <span className="job-title">
-کارشناس پشتیبانی و توسعه وردپرس
-</span>
-<span className="ad-age">
-  (5 روز پیش)
-</span>
+        <div className="row1">
+          <div className="company-img">
+            <img src={logo} />
+          </div>
+          <div className="row1-content">
+            <div className="row1-content-row">
+              <div className="job-title-container">
+                <span className="job-title">
+                  کارشناس پشتیبانی و توسعه وردپرس
+                </span>
+                <span className="ad-age">(5 روز پیش)</span>
+              </div>
+              <div className="icons-container">
+                <a href="">
+                  <HiOutlineShare className="share-icon"></HiOutlineShare>
+                </a>
+                <a href="">
+                  <BsHeart className="like-icon"></BsHeart>
+                </a>
+              </div>
             </div>
-            <div className="icons-container">
-              <a href=""><HiOutlineShare className="share-icon"></HiOutlineShare></a>
-              <a href=""><BsHeart className="like-icon"></BsHeart></a>
+            <div className="row1-content-row">
+              <span className="company-name">آنومالی</span>
+            </div>
+            <div className="row1-content-row">
+              <span className="address">تهران، آیت الله کاشانی</span>
+              <span className="cooperation-type">تمام وقت</span>
             </div>
           </div>
-          <div className="row1-content-row">
-            <span className="company-name">آنومالی</span>
+        </div>
+        <div className="row2">
+          <div>
+            <span>آدرس</span>
+            <p>تهران، آیت الله کاشانی</p>
           </div>
-          <div className="row1-content-row">
-            <span className="address">
-            تهران، آیت الله کاشانی
-            </span>
-            <span className="cooperation-type">
-              تمام وقت
-            </span>
+          <div>
+            <span>نوع همکاری</span>
+            <p>تمام وقت</p>
           </div>
-        </div>
-       </div>
-       <div className="row2">
-        <div>
-          <span>
-            آدرس
-          </span>
-          <p>
-          تهران، آیت الله کاشانی
-          </p>
-        </div>
-        <div>
-          <span>
-            نوع همکاری
-          </span>
-          <p>
-          تمام وقت
-          </p>
-        </div>
-<div>
-<span>
-روز و ساعت کاری
-</span>
-<p>
-شنبه تا چهارشنبه 9 الی 17:15-پنجشنبه ها 9 الی 13:15
-</p>
-</div>
-<div>
-<span>
-  سفر های کاری
-</span>
-<p>
-  -
-</p>
-</div>
+          <div>
+            <span>روز و ساعت کاری</span>
+            <p>شنبه تا چهارشنبه 9 الی 17:15-پنجشنبه ها 9 الی 13:15</p>
+          </div>
+          <div>
+            <span>سفر های کاری</span>
+            <p>-</p>
+          </div>
 
-<div>
-<span>
-مزایا و تسهیلات
-</span>
-<p>
-بیمه درمان تکمیلی -
-پزشک سازمانی
-</p>
-</div>
-       </div>
+          <div>
+            <span>مزایا و تسهیلات</span>
+            <p>بیمه درمان تکمیلی - پزشک سازمانی</p>
+          </div>
+        </div>
       </div>
-<div className="sticky-job-header">
-  <div className="sticky-icons-container">
-    <a href=""><BsHeart></BsHeart></a>
-    <a href=""><HiOutlineShare></HiOutlineShare></a>
-  </div>
-  <div className="sticky-title-container">
-    <span>
-    کارشناس پشتیبانی و توسعه وردپرس
-    </span>
-    <span>
-      آنومالی
-    </span>
-  </div>
-  <div className="sticky-back-button-container">
-     <a href=""><SlArrowLeft></SlArrowLeft></a>
-  </div>
-</div>
-    </div>;
-  };
-  export default JobAdHeader;
+      <div className="sticky-job-header">
+        <div className="sticky-icons-container">
+          <a href="">
+            <BsHeart></BsHeart>
+          </a>
+          <a href="">
+            <HiOutlineShare></HiOutlineShare>
+          </a>
+        </div>
+        <div className="sticky-title-container">
+          <span>کارشناس پشتیبانی و توسعه وردپرس</span>
+          <span>آنومالی</span>
+        </div>
+        <div className="sticky-back-button-container">
+          <a href="">
+            <SlArrowLeft></SlArrowLeft>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default JobAdHeader;
--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,45 +1,33 @@
-import "../style/login.css"
-import logo from "../assets/logo-big.png"
-import { Link } from "react-router-dom";
-const Login = ({userType}) => {
-  return <div className="login-body">
-    <div className="login-container">
-    <div className="login-wrapper">
-      <div className="login">
-      <div className="login-logo">
-            <img src={logo} alt="login logo"/>
-           </div>
-           <div className="login-header">
-            <span className="login-header-title">
-              ورود | ایجاد حساب
-            </span>
+import "../style/login.css";
+import logo from "../assets/img/logo-big.png";
+const Login = ({ userType }) => {
+  return (
+    <div className="login-body">
+      <div className="login-container">
+        <div className="login-wrapper">
+          <div className="login">
+            <div className="login-logo">
+              <img src={logo} alt="login logo" />
+            </div>
+            <div className="login-header">
+              <span className="login-header-title">ورود | ایجاد حساب</span>
 
-            <span className="login-user-type">{userType}</span>
-           </div>
-           <div className="login-email-form">
-            <label>
-              آدرس ایمیل خودتان را وارد کنید
-            </label>
-            <input type="email" placeholder="example@gmail.com"/>
-            <input type="button" value="ادامه" />
-           </div>
+              <span className="login-user-type">{userType}</span>
+            </div>
+            <div className="login-email-form">
+              <label>آدرس ایمیل خودتان را وارد کنید</label>
+              <input type="email" placeholder="example@gmail.com" />
+              <input type="button" value="ادامه" />
+            </div>
+          </div>
+          <button className="user-switch">
+            {userType === "کارجو" ? "کارفرما" : "کارجو"} هستید؟
+          </button>
+        </div>
       </div>
-           <button className="user-switch">
-            
-            {userType==="کارجو"?"کارفرما":"کارجو"} هستید؟
-           </button> 
     </div>
-
-
-   
-
-  </div>
-  </div>
-  
+  );
 };
 
-
-function hamed(){
-  
-}
+function hamed() {}
 export default Login;
