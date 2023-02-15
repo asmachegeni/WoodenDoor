84e33c5e1e7eab88a21a71b33c95790d86651dc1
HamedWS <HamedWosugy@gmail.com>
2023-02-15 14:52:26 +0330

    Add TopCategories

--- src/component/Blog/TopCategories.jsx
+++ src/component/Blog/TopCategories.jsx
@@ -1,7 +1,28 @@
+import "../../style/Blog/TopCategories.css";
+import ExampleImage from "../../assets/img/TopCategorieExample.jpg";
 const TopCategories = () => {
-    return <div>
-
-    </div>;
+  return (
+    <div className="TopCategories">
+      <div className="CategorieBox">
+        <a href="#">
+          <img src={ExampleImage} />
+          <span>رزومه</span>
+        </a>
+      </div>
+      <div className="CategorieBox">
+        <a href="#">
+          <img src={ExampleImage} />
+          <span>مصاحبه شغلی</span>
+        </a>
+      </div>
+      <div className="CategorieBox">
+        <a href="#">
+          <img src={ExampleImage} />
+          <span>حقوق و دستمزد</span>
+        </a>
+      </div>
+    </div>
+  );
 };
 
-export default TopCategories
+export default TopCategories;
