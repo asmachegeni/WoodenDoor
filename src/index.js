3a5b169ee69c53b0b9bc6ae1f6fb2deed415a221
asma <as.ch.2280@gmail.com>
2023-02-17 01:12:24 +0330

    add register employee component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Login from './component/Login';
-import SendingResume from './component/SendingResume';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Login from "./component/Login";
+import SendingResume from "./component/SendingResume";
+import Employee from "./component/RegistersForm/Employee";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SendingResume />
+    <Employee />
   </React.StrictMode>
 );
 
